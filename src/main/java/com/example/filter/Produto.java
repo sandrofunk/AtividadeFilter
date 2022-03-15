ea40124952209df181ac7fbca4030e9f35d841dc
sandrofunk <sandro.funk@bbchain.com.br>
2022-03-14 23:15:56 -0300

    Classe Produto

---
+++ src/main/java/com/example/filter/Produto.java
@@ -0,0 +1,20 @@
+package com.example.filter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Produto {
+
+	final String nome;
+	final double preco;
+	final double desconto;
+	final double valorFrete;
+
+	public Produto(String nome, double preco, double desconto, double valorFrete) {
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+		this.valorFrete = valorFrete;
+
+	}
+}
